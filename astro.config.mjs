d05d7e6c0b77dcc809a107617580e298f6860f3e
thomasrobveille <thomasrobveille@gmail.com>
2024-10-16 02:57:46 +0200

    Ajout îles composant UI Fin tuto astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://charming-babka-fc8544.netlify.app/"
+  site: "https://charming-babka-fc8544.netlify.app/",
+  integrations: [preact()]
 });
